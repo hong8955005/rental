b4b76dab22b5e44f7ffcf405ae80008f6aa9cbc5
hong8955005 <104149567+hong8955005@users.noreply.github.com>
2022-09-13 12:20:32 +0800

    Update serviceData.js

--- src/assets/data/serviceData.js
+++ src/assets/data/serviceData.js
@@ -1,44 +1,44 @@
 const serviceData = [
   {
     id: 1,
-    title: "City Transfer",
-    icon: "ri-map-pin-2-line",
-    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
+    title: "Wide Selection",
+    icon: "ri-roadster-line",
+    desc: "We provide all kinds of vehicles for your travel need. From sedan to SUV, we have it all.",
   },
 
   {
     id: 2,
-    title: "Whole  City Tour",
-    icon: "ri-community-line",
-    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
+    title: "Fast Pickup",
+    icon: "ri-flight-takeoff-line",
+    desc: "Our team will send out the vehicle according to the time and location at your convenience.",
   },
 
   {
     id: 3,
-    title: "Unlimited Miles Car Rental",
-    icon: "ri-roadster-line",
-    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
+    title: "Breakdown Support",
+    icon: "iconsax-lin-24-support",
+    desc: "24-hour breakdown assistance during the rental period should you need any assistance..",
   },
 
   {
     id: 4,
-    title: "Fast & Easy Booking",
-    icon: "ri-timer-flash-line",
-    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
+    title: "Trusted Company",
+    icon: "fas-handshake-angle",
+    desc: "MIN Transportation established since 2009.",
   },
 
   {
     id: 5,
-    title: "Many Pickup Locations",
-    icon: "ri-map-pin-line",
-    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
+    title: "No Hidden Costs",
+    icon: "gmdi-monetization-on-o",
+    desc: "Clear price breakdowns, no GST. All inclusive rates.",
   },
 
   {
     id: 6,
-    title: "Airport Transfer",
-    icon: "ri-flight-takeoff-line",
-    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
+    title: "Best Price Guaranteed",
+    icon: "ri-award-fill",
+    desc: "Compare your car rental online with best prices guaranteed.",
   },
 ];
 
