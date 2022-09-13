eb182a88e822d4c57e5b2d938e74e9df4399ae42
hong8955005 <104149567+hong8955005@users.noreply.github.com>
2022-09-13 13:51:01 +0800

    Update carData.js

--- src/assets/data/carData.js
+++ src/assets/data/carData.js
@@ -9,7 +9,8 @@ import img07 from "../all-images/cars-img/toyota-offer-2.png";
 import img08 from "../all-images/cars-img/mercedes-offer.png";
 
 const carData = [
-  {
+  
+	  {
     id: 1,
     brand: "Tesla",
     rating: 112,
@@ -24,118 +25,408 @@ const carData = [
     description:
       " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
   },
-
-  {
+	
+	  {
     id: 2,
-    brand: "Toyota",
-    rating: 102,
-    carName: "Toyota Aventador",
-    imgUrl: img02,
-    model: "Model-2022",
-    price: 50,
-    speed: "20kmpl",
+    brand: "Tesla",
+    rating: 112,
+    carName: "PERODUA VIVA",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
     gps: "GPS Navigation",
     seatType: "Heated seats",
     automatic: "Automatic",
     description:
       " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
   },
-
-  {
+	
+	
+	  {
     id: 3,
-    brand: "BMW",
-    rating: 132,
-    carName: "BMW X3",
-    imgUrl: img03,
-    model: "Model-2022",
-    price: 65,
-    speed: "20kmpl",
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
     gps: "GPS Navigation",
     seatType: "Heated seats",
     automatic: "Automatic",
     description:
       " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
   },
-
-  {
+	
+	
+	  {
     id: 4,
-    brand: "Nissan",
-    rating: 102,
-    carName: "Nissan Mercielago",
-    imgUrl: img04,
-    model: "Model-2022",
-    price: 70,
-    speed: "20kmpl",
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
     gps: "GPS Navigation",
     seatType: "Heated seats",
     automatic: "Automatic",
     description:
       " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
   },
-
-  {
+	
+	
+	  {
     id: 5,
-    brand: "Ferrari",
-    rating: 94,
-    carName: "Ferrari Camry",
-    imgUrl: img05,
-    model: "Model-2022",
-    price: 45,
-    speed: "20kmpl",
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
     gps: "GPS Navigation",
     seatType: "Heated seats",
     automatic: "Automatic",
     description:
       " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
   },
-
-  {
+	
+	
+	  {
     id: 6,
-    brand: "Mercedes",
-    rating: 119,
-    carName: "Mercedes Benz XC90",
-    imgUrl: img06,
-    model: "Model-2022",
-    price: 85,
-    speed: "20kmpl",
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
     gps: "GPS Navigation",
     seatType: "Heated seats",
     automatic: "Automatic",
     description:
       " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
   },
-
-  {
+	
+	
+	  {
     id: 7,
-    brand: "Audi",
-    rating: 82,
-    carName: "Audi Fiesta",
-    imgUrl: img07,
-    model: "Model 3",
-    price: 50,
-    speed: "20kmpl",
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
     gps: "GPS Navigation",
     seatType: "Heated seats",
     automatic: "Automatic",
     description:
       " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
   },
-
-  {
+	
+	
+	  {
     id: 8,
-    brand: "Colorado",
-    rating: 52,
-    carName: "Rolls Royce Colorado",
-    imgUrl: img08,
-    model: "Model 3",
-    price: 50,
-    speed: "20kmpl",
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
     gps: "GPS Navigation",
     seatType: "Heated seats",
     automatic: "Automatic",
     description:
       " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
   },
+	
+	  {
+    id: 9,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+  
+	  {
+    id: 10,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+  
+  
+  {
+    id: 11,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+	
+	  {
+    id: 12,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+	
+	
+	  {
+    id: 13,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+	
+	
+	  {
+    id: 14,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+	
+	
+	  {
+    id: 15,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+	
+	
+	  {
+    id: 16,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+	
+	
+	  {
+    id: 17,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+	
+	
+	  {
+    id: 18,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+	
+	  {
+    id: 19,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+  
+	  {
+    id: 20,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+  
+  
+  {
+    id: 21,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+	
+	  {
+    id: 22,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+	
+	
+	  {
+    id: 23,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+	
+	
+	  {
+    id: 24,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+	
+	
+	  {
+    id: 25,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Perodua Kelisa",
+    imgUrl: img01,
+    model: "Hatchback",
+    price: "5x Passenger",
+    speed: "1.0 CC Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+	
 ];
 
 export default carData;
