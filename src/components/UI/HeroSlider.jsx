68f90a0cbe5cc5c3d4e10148cf74b478d609e01b
hong8955005 <104149567+hong8955005@users.noreply.github.com>
2022-09-13 10:55:16 +0800

    Update HeroSlider.jsx

--- src/components/UI/HeroSlider.jsx
+++ src/components/UI/HeroSlider.jsx
@@ -22,11 +22,13 @@ const HeroSlider = () => {
       <div className="slider__item slider__item-01 mt0">
         <Container>
           <div className="slider__content ">
-            <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
-            <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
+             <h4 className="text-light mb-3">We provide the cheapest car rental service in Kuching, Sarawak.</h4>
+            <h1 className="text-light mb-4">MIN TRANSPORTATION</h1>
+
+            
 
             <button className="btn reserve__btn mt-4">
-              <Link to="/cars">Reserve Now</Link>
+              <Link to="/cars">View All Cars</Link>
             </button>
           </div>
         </Container>
@@ -35,11 +37,10 @@ const HeroSlider = () => {
       <div className="slider__item slider__item-02 mt0">
         <Container>
           <div className="slider__content ">
-            <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
-            <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
-
+            <h4 className="text-light mb-3">Ensuring quality and efficiency for all our customers, at all times</h4>
+            <h1 className="text-light mb-4">BEST IN KUCHING</h1>
             <button className="btn reserve__btn mt-4">
-              <Link to="/cars">Reserve Now</Link>
+              <Link to="/cars">View All Cars</Link>
             </button>
           </div>
         </Container>
@@ -48,11 +49,11 @@ const HeroSlider = () => {
       <div className="slider__item slider__item-03 mt0">
         <Container>
           <div className="slider__content ">
-            <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
-            <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
+           <h4 className="text-light mb-3">Car, Van and Bus Rentals. Travel & Tour</h4>
+            <h1 className="text-light mb-4">OUR SERVICES</h1>
 
             <button className="btn reserve__btn mt-4">
-              <Link to="/cars">Reserve Now</Link>
+              <Link to="/cars">View All Cars</Link>
             </button>
           </div>
         </Container>
