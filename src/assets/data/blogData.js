3df07322c7e204bad273be3489e1c37fb3d6706c
hong8955005 <104149567+hong8955005@users.noreply.github.com>
2022-09-13 14:31:20 +0800

    Update blogData.js

--- src/assets/data/blogData.js
+++ src/assets/data/blogData.js
@@ -6,41 +6,41 @@ import img03 from "../all-images/blog-img/blog-3.jpg";
 const blogData = [
   {
     id: 1,
-    title: "The best way to drive cars",
+    title: "DISCOVER KUCHING",
     author: "Muhib",
     date: "12 Dec, 2020",
     time: "9pm",
     imgUrl: img01,
     description:
-      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+      " Revealing a side of Asia unlike anywhere else, Sarawak offers an alternative for those seeking humble authenticity far from the tourist-trap clichés. To discover Sarawak means taking the road less travelled. Whether it is sharing in lifestyle of one of the many indigenous communities longhouses, exploring gigantic caves in the UNESCO World Heritage of Gunung Mulu National Park, you will find Sarawak offers a host of memorable experiences to bring home to. As part of Malaysia on the island of Borneo, Sarawak is a peaceful and stable place for travel, suited for large groups or single travellers alike.",
     quote:
-      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+      "Youtube Video: https://www.youtube.com/watch?v=cy89XhJlJuc",
   },
 
   {
     id: 2,
-    title: "If your car battery is down",
+    title: "MALAYSIA DAY CAR RENTAL PROMO",
     author: "Muhib",
     date: "12 Dec, 2020",
     time: "9pm",
     imgUrl: img02,
     description:
-      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+      " Malaysia Day Promo with lowest rate, special discounts and freebies from us. Booking period for this promo is from 16th September 26th September. We offer you competitive prices and up-to-date information, ensuring quality and efficiency for all our visitors, at all times. Book your rental car in Kuching today and enjoy your holiday, or get anywhere in time for your business meetings. At Iman Car Rental, we understand that travelling can be a hassle if you are not well organized, which is why we provide solutions in the form of a variety of car types to compare and choose from to cater to all types of travellers. Your next cheap car hire in Kuching is now just a few clicks away, at Min Transportation.",    
     quote:
-      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+      "You can call us directly at 017 844 8882(Ah Min).",
   },
 
   {
     id: 3,
-    title: "The best way to give trip",
+    title: "PICK-UP & DROP-OFF AT KUCHING AIRPORT",
     author: "Muhib",
     date: "12 Dec, 2020",
     time: "9pm",
     imgUrl: img03,
     description:
-      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+      " Never before has booking a rental car in Kuching been easier with Min Transportation. Each rental car in Kuching has a great and affordable rate, as well as we offer a variety of pick-up and drop-off options to make your life more convenient. When travelling, go with confidence with Iman Car Rental. At Iman Car Rental, our goal is to offer you a variety of Kuching car hire discounts at the very best price to suit our customer's needs. We offer car hire options in Kuching from compact economy cars with low consumption to luxury or off road solutions, depending on your preference. Every Kuching rental car we feature will come fully equipped with the latest amenities and safety features that ensure your trips will be safe, convenient and comfortable.",
     quote:
-      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+      "It has never been easier to find a rental car in Kuching.",
   },
 ];
 
