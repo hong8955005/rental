79658947f16b17190653fca53baf5addffa5db4c
hong8955005 <104149567+hong8955005@users.noreply.github.com>
2022-09-14 10:26:08 +0800

    Update CarListing.jsx

--- src/pages/CarListing.jsx
+++ src/pages/CarListing.jsx
@@ -13,7 +13,8 @@ const CarListing = () => {
       <section>
         <Container>
           <Row>
-            <Col lg="12">
+             {/* ============
+             <Col lg="12">
               <div className=" d-flex align-items-center gap-3 mb-5">
                 <span className=" d-flex align-items-center gap-2">
                   <i class="ri-sort-asc"></i> Sort By
@@ -26,7 +27,7 @@ const CarListing = () => {
                 </select>
               </div>
             </Col>
-
+ ============ */}
             {carData.map((item) => (
               <CarItem item={item} key={item.id} />
             ))}
